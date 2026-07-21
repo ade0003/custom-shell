66544c84cb69f91a3364ec38fe43326d6f748d24
Goodness Ade <ade00003@algonquinlive.com>
2026-07-21 00:48:52 -0400

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,8 @@ class Program
 {
     static void Main()
     {
-        Console.Write("$ ");
+        Console.Write("$");
+        string command = Console.ReadLine();
+        Console.Write($"{command}: command not found")
     }
 }
